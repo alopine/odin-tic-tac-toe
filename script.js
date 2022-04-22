aecd2b69a5d82abdb37488d251dfafec9bae73b3
alopine <loariel98@gmail.com>
2022-04-22 11:01:46 -0700

    Add logic for checking winner

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
+// Main objects
+
 const gameBoard = (() => {
+    // Initialize game board
     let board = [...Array(3)].map(() => Array(3).fill(""));
-    return {board};
+
+    // Check for winner
+    const checkWinner = () => {
+        // Declare variables
+        let winStatus = false;
+        let winner;
+        // Check horizontal and vertical rows
+        for (let i = 0; i < 3; i++) {
+            if ((board[i][0] !== "") && (winStatus = board[i][0] === board[i][1] && board[i][0] === board[i][2])) {
+                winner = (board[i][0] === playerOne.marker) ? playerOne : playerTwo;
+                break;
+            } else if ((board[0][i] !== "") && (winStatus = board[0][i] === board[1][i] && board[0][i] === board[2][i])) {
+                winner = (board[0][i] === playerOne.marker) ? playerOne : playerTwo;
+                break;
+            }
+        }
+        // If no winner][check diagonal rows
+        if (!winStatus && board[1][1] !== "") {
+            if (winStatus = (board[1][1] === board[0][0] && board[1][1] === board[2][2]) || (board[1][1] === board[0][2] && board[1][1] === board[2][0])) {
+                winner = (board[1][1] === playerOne.marker) ? playerOne : playerTwo;
+            }
+        }
+        // Return variables
+        return [winStatus, winner];
+    }
+
+    return {
+        board,
+        checkWinner
+    };
 })();
 
-
-// displayController object module
-        // renderGameBoard function
-            // Add game table
-            // Loop to insert 3 rows
-                // Loop to insert three cells per row
-                    // Add event listener click, renderMove
-        // renderMove function
-            // Check if clicked cell already has content (ie if gameBoard.board[rowIndex][cellIndex] is empty or not)
-                // If empty
-                    // Check whose turn it is and what their symbol is
-                    // Insert symbol into gameBoard.board[rowIndex][cellIndex] and into clicked cell
-        // trackTurn function
 
 const displayController = (() => {
     // Render game board on page
@@ -35,44 +54,45 @@ const displayController = (() => {
     }
     // Render player move on game board
     const renderMove = (evt) => {
-        // Check clicked cell for content
+        // Check for clicked cell content and win conditions met
         const clickedCell = evt.currentTarget;
-        if (!clickedCell.textContent) {
-            // Plug current player symbol into gameBoard.board
-            gameBoard.board[clickedCell.parentNode.rowIndex][clickedCell.cellIndex] = trackTurn().symbol;
-            clickedCell.textContent = trackTurn().symbol;
+        if (!clickedCell.textContent && !gameBoard.checkWinner()[0]) {
+            // Place current player marker into array and game board space
+            gameBoard.board[clickedCell.parentNode.rowIndex][clickedCell.cellIndex] = currentPlayer().marker;
+            clickedCell.textContent = currentPlayer().marker;
+            // Update current turn
             currentTurn++;
         } else {
-            window.alert("Space already filled!");
+            return;
         }
 
     }
     // Track current turn
     let currentTurn = 1;
-    const trackTurn = () => {
+    const currentPlayer = () => {
         if (currentTurn % 2 !== 0) {
             return playerOne;
         } else {
             return playerTwo;
         }
     }
+
     return {
         renderGameBoard,
         renderMove,
-        trackTurn
+        currentPlayer
     };
 })();
 
-// player object factory
-    // name
-    // X or O
 
-const player = (name, symbol) => {
-    return {name, symbol};
+const player = (name, marker) => {
+    return {name, marker};
 };
 
 const playerOne = player("", "X");
 const playerTwo = player("", "O");
+
+
 
 // Event listeners
 
@@ -85,7 +105,7 @@ startButton.addEventListener("click", () => {
         window.alert("Please fill in the names of both players.")
     } else {
         displayController.renderGameBoard();
-        playerOne.name = inputPlayerOne.textContent;
-        playerTwo.name = inputPlayerTwo.textContent;
+        playerOne.name = inputPlayerOne.value;
+        playerTwo.name = inputPlayerTwo.value;
     }
 })
